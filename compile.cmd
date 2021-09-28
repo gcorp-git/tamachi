83361163df0d63a2f8d9aa33ac2810d130945bcd
gcorp-git <gcorp.gcorp@gmail.com>
2021-09-28 19:38:42 +0300

    initial commit

---
+++ compile.cmd
@@ -0,0 +1,5 @@
+@echo off
+if not exist ".\bin" mkdir ".\bin"
+pushd ".\bin"
+call g++ -std=c++17 -O3 "..\src\main.cpp" -o "tamachi.exe" -luser32 -lgdi32
+popd
---
+++ run.cmd
@@ -0,0 +1,4 @@
+@echo off
+pushd ".\bin"
+call ".\tamachi.exe"
+popd
